e4ff874e1ccf5c144b823bd525619351580ef89d
Vikram Kumar <vikrambr8084@gmail.com>
2024-02-02 23:58:30 +0530

    to find lcm

---
+++ LCM.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int lcm(int a,int b){
+    int res=max(a,b);
+    while(true){
+        if(res%a==0&&res%b==0)
+        return res;
+        res++;
+    }
+    return res;
+}
+int main(){
+    int a,b;
+    cout<<"Enter the two number:"<<endl;
+    cin>>a>>b;
+    cout<<"THE LCM OF TWO NUMBER IS:"<<lcm(a,b)<<endl;
+    return 0;
+}
