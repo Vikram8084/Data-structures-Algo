8b5360614a32acdcd30250917391f14cecf2f1e1
Vikram Kumar <vikrambr8084@gmail.com>
2024-02-16 23:58:37 +0530

    array level 1

--- week 3/02_array level 1/11_array.cpp
+++ week 3/02_array level 1/11_array.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
+#include<limits.h>
 using namespace std;
 int main(){
+    // -2 to the pwer 31
+    cout<<INT_MIN<<endl;
+    // 2to the power 31
+    cout<<INT_MAX<<endl;
+    
+
     
 }
