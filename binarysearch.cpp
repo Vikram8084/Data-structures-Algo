7932efb98659f88e73848253eddb25c19569c20b
Vikram Kumar <vikrambr8084@gmail.com>
2024-02-27 23:54:28 +0530

    binary search problem

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -20,6 +20,7 @@ using namespace std;
         return -1;
     }
     int main(){
+        
         int even[6]={2,3,4,5,6,7};
         int odd[5]={1,3,5,7,9};
         int index=binarySearch(even,6,6);
