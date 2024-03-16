37596679fafe77acc6126cf068a07c6586d5be2e
Vikram Kumar <vikrambr8084@gmail.com>
2024-03-16 22:24:42 +0530

    searching & sorting

--- Week 4/Searching &sorting/seaeching.cpp
+++ Week 4/Searching &sorting/seaeching.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
+int linearsearch(int arr[],int n,int target){
+    for(int i=0;i<n;i++){
+        if(arr[i]==target)
+        return i;
+    }
+}
 int main(){
     int arr[]={20,30,10,40,50};
+    int n=5;
+    int target=50;
+    int ans=linearsearch(arr,5,50);
+    cout<<ans;
     return 0;
     
 }
